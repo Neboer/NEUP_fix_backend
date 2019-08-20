ad4bb9c9e6da37e89a9019745ba267903b06312a
neboer <rubinposter@gmail.com>
2019-08-20 22:18:38 +0800

    一些改动。大量的api实现，发现了大量的问题，联合文档一并修改。

--- app.js
+++ app.js
@@ -24,15 +24,23 @@ client.connect().then((Client) => {
     app.get('/announcement', (req, res) => {
         announcement.find({}).toArray(((error, result) => {
             let pass_result = result.map((single_ann) => {
-                return {"annid": single_ann._id, "text": single_ann.text, "image": single_ann.image}
+                return {
+                    "annid": single_ann._id,
+                    "text": single_ann.text,
+                    "image": single_ann.image,
+                    "anntime": single_ann.anntime
+                }
             });
             res.json(pass_result);
         }))
     });
+    // 添加一个公告
     app.post('/announcement', validate({body: schema.announcement_post_body}), ((req, res) => {
-        announcement.insertOne(req.body).catch((reason => {
+        let prepared_announcement = req.body;
+        prepared_announcement.anntime = new Date();
+        announcement.insertOne(prepared_announcement).catch((reason => {
             throw reason;
-        }));// TODO: add result judgement.加一个萌萌哒夹击妹抖
+        }));
         res.status(200).end();
     }));
     app.delete('/announcement/:annid', (req, res) => {
@@ -89,14 +97,90 @@ client.connect().then((Client) => {
         })
     });
     app.get('/app', validate({query: schema.app_get_query}), (req, res) => {
-        appointment.find(req.query).toArray((error, result) => {
-            let find_result = result.map((one_appointment) => {
-                delete one_appointment._id;
-                return one_appointment;
+        appointment.find(req.query).toArray((error, result) => {// TODO: 这里不能仅仅拿掉id就返回给用户。
+            let prepared_result = result.map((one_appointment) => {
+                return {
+                    appid: one_appointment.appid,
+                    status: one_appointment.status,
+                    userid: one_appointment.userid,
+                    app_make_time: one_appointment.app_make_time,
+                    repair: one_appointment.repair,
+                    device_type: one_appointment.device_type,
+                    device_model: one_appointment.device_model
+                }
             });
-            res.json(find_result);
+            res.json(prepared_result);
         })
     });
+
+    app.post('/app', validate({body: schema.app_post_body}), ((req, res) => {
+        let insert_data = req.body;
+        insert_data.appid = 2; // 自增运算！
+        insert_data.status = "submitted";
+        insert_data.app_exec_time = null;
+        insert_data.member = null;// submitted状态下，还没有安排维修人员。
+        insert_data.history = [{
+            status: "submitted",
+            userid: "2018XXXX",
+            time: new Date()
+        }];
+        insert_data.mesid_list = [];// 留言板id列表。这个作用非常明确，绑定每个预约和对应的留言板id。留言板不用一并发送给客户端，客户端需要访问/
+        // 至此，一个预约的所有元数据准备完毕，存入数据库。
+        appointment.insertOne(insert_data).then(() => {
+            res.status(200).end("insert successful.")
+        }).catch((error) => {
+            console.error(error)
+        })
+    }));
+
+    app.get('/app/:appid', (req, res) => {// 获取一个预约的详细信息。
+        appointment.find({appid: req.params.appid}).toArray(((error, result) => {
+            let full_data = result[0];
+            delete full_data._id;
+            delete full_data.mesid_list;
+            res.json(full_data);
+        }))
+    });
+
+    app.patch('/app/:appid', validate({body: schema.app_update_body}), (req, res) => {
+        // 检查update行为是否合法。不能对一个已经被标为“成功/失败/取消”的目标update
+        appointment.find({appid: req.query.appid}).toArray(((error, result) => {
+            if (result.length === 0) {
+                res.status(410).end("no such appointment");
+            }
+            // 只有已提交或已接受的记录才可以更新。为了便于理解，我们把状态分为三类“已提交”、“已接受”和“已结束”。状态只能向前更新。
+            if (result[0].status)
+            if (result[0].status !== "submitted" && result[0].status !== "accepted") {
+                res.status(402).end("cannot update a terminated appointment");
+            }
+            if (result[0].status === "accepted")
+        }));
+        appointment.updateOne({appid: req.query.appid}, {$set: req.body}).then((result) => {
+            if (result.result.nModified === 1) {
+                res.status(200).end("update successful.")
+            } else if (result.result.n === 1) {
+                res.status(200).end("no update performed");
+            }
+        });
+    });
+
+    app.put('/app/:appid', validate({body: schema.app_post_body}), (req, res) => {
+        appointment.updateOne({appid: req.query.appid}, {$set: req.body}).then((result) => {
+            if (result.result.nModified === 1) {
+                res.status(200).end("update successful.")
+            } else if (result.result.n === 1) {
+                res.status(200).end("no update performed");
+            } else {
+                res.status(410).end('no such appointment')
+            }
+        })
+    });
+
+    app.delete('/app/:appid', (req, res) => {
+        appointment.updateOne({appid: req.query.appid}, {
+            $set: {}
+        })
+    })
     app.use((err, req, res, next) => {
         if (err instanceof ValidationError) {
             // At this point you can execute your error handling code
--- validate_schema.js
+++ validate_schema.js
@@ -39,18 +39,60 @@ let schema = {
     app_get_query: {
         type: "object",
         properties: {
-            appid: {type: "string"},
+            appid: {type: "string"},// 虽然appid看起来是integer，但是由于query的特点，一切皆string
             status: {
                 type: "string",
                 enum: ["submitted", "successful", "failed", "canceled"]
             },
             userid: {type: "string"},
-            app_make_time:{type:"string"},
-            repair:{type:"string"},
-            device_type:{type:"string"},
-            device_model:{type:"string"}
+            app_make_time: {type: "string", format:"date-time"},
+            repair: {type: "string"},
+            device_type: {type: "string"},
+            device_model: {type: "string"}
         },
         additionalProperties: false
+    },
+
+    // 提交新的预约时用到的
+    app_post_body: {
+        type: "object",
+        properties: {
+            userid: {type: "string"},
+            device_type: {type: "string"},
+            device_model: {type: "string"},
+            repair: {type: "string"},
+            describe: {type: "string"},
+            contact: {
+                type: "array",
+                items: [{
+                    type: "string",
+                }, {
+                    type: "string"
+                }],
+                additionalItems: false
+            },
+            contact_time:{type:"string"},
+            site:{type:"string"}
+        },
+        additionalProperties:false
+    },
+
+    // 修改预约属性
+    app_update_body: {
+        type: "object",
+        properties:{
+            status: {
+                type: "string",
+                enum: ["successful", "failed", "accepted"]
+            },
+            app_exec_time:{
+                type: "string",
+                format: "date-time"
+            },
+            member:{type:"string"},
+            site:{type:"string"}
+        },
+        additionalProperties:false
     }
 };
 export default schema;
